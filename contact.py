acf5f8eb0f8ad67562e21eb1eb8b8103ea68d98c
Andrey <it.peskov@gmail.com>
2020-02-26 20:04:23 +0300

    Задание №3: Сделать всё то же самое, но для контактов

---
+++ contact.py
@@ -0,0 +1,11 @@
+__author__ = 'andrey'
+
+
+class Contact:
+
+    def __init__(self,firstname, lastname, address, mobile, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.mobile = mobile
+        self.email = email
