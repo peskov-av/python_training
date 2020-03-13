6b4ab97f1f49d8aebd8eaebf519758e1934c8867
andrej <it.peskov@gmail.com>
2020-03-13 13:14:16 +0300

    Повышаем "интеллектуальность" навигации: избавляемся от лишних переходов

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from fixture.application import Application
 
 fixture = None
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def app(request):
     global fixture
     if fixture is None:
--- fixture/group.py
+++ fixture/group.py
@@ -8,6 +8,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
+        if wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0:
+            return
         wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
